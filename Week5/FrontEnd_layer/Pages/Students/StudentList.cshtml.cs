6fb39193e761cfa261076f7edaccb9362f009473
natcharapaphon <6431503019@lamduan.mfu.ac.th>
2025-03-10 11:45:52 +0700

    save 1

---
+++ Week5/FrontEnd_layer/Pages/Students/StudentList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class StudentListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
